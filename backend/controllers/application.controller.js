894d60231b5364371530433ce188ba029aee4754
Ajay <ajaykathar30gmail.com>
2025-06-28 08:42:06 +0530

    fixed error

--- backend/controllers/application.controller.js
+++ backend/controllers/application.controller.js
@@ -44,7 +44,7 @@ export const getApplications=async(req,res)=>{
         if(applications.length===0){
             return res.status(404).json({message:"No applications found",success:false})
         }   
-        return res.status(200).json({message:"Applications found successfully",applications,success:true})
+        return res.status(200).json({message:"Applications found",applications,success:true})
     }catch (error) {
         console.error(error)
         return res.status(500).json({ message: "Internal server error", success: false })
